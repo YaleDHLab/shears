a17c8442f9edf33bf9b032fe2960ccc7ada35a91
duhaime <douglas.duhaime@gmail.com>
2019-05-24 12:22:41 -0400

    control namespace

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup
 
 setup (
   name='picsnip',
-  version='0.0.1',
+  version='0.0.2',
   packages=['picsnip'],
   keywords = ['machine-vision', 'computer-vision', 'image-processing', 'data-collection'],
   description='Extract illustrations from book page scans',
