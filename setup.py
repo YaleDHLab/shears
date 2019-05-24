45d1b820472923c6660c56e34dd6a65667ac6b02
duhaime <douglas.duhaime@gmail.com>
2019-05-24 10:43:22 -0400

    iniital commit of setup utils

---
+++ setup.py
@@ -0,0 +1,21 @@
+from setuptools import setup
+
+setup (
+  name='picsnip',
+  version='0.0.1',
+  packages=['picsnip'],
+  keywords = ['machine-vision', 'computer-vision', 'image-processing', 'data-collection'],
+  description='Extract illustrations from book page scans',
+  url='https://github.com/yaledhlab/picsnip',
+  author='Douglas Duhaime',
+  author_email='douglas.duhaime@gmail.com',
+  license='MIT',
+  install_requires=[
+    'glob2>=0.6',
+    'matplotlib>=3.0.3',
+    'scikit-image>=0.15.0',
+    'scikit-learn>=0.20.3',
+    'scipy>=1.2.1',
+  ],
+)
+
